9b4421c74c53ff970f4d2b5387e016c5c5c7e4d7
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-17 20:34:46 +0100

    M1.01 Adott három szám. Határozza meg átlagukat és írja ki a képernyőre az eredményt!

--- 01-03-01-average-of-two-numbers-round/src/org/example/MyAverageOfNumbers.java
+++ 01-03-01-average-of-two-numbers-round/src/org/example/MyAverageOfNumbers.java
@@ -4,7 +4,10 @@ public class MyAverageOfNumbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// M1.01 Adott három szám. Határozza meg átlagukat és írja ki a képernyõre az eredményt!
+		int a=5;
+		int b=4;
+		int c=2;
 	}
 
 }
