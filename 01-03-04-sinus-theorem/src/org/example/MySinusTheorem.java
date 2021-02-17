296ca6448783f9e28664b211071cf6f76f740181
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-17 21:06:37 +0100

    M4.01 Matematikai feladat megoldása sinus tétellel

--- 01-03-04-sinus-theorem/src/org/example/MySinusTheorem.java
+++ 01-03-04-sinus-theorem/src/org/example/MySinusTheorem.java
@@ -4,6 +4,8 @@ public class MySinusTheorem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// M4.01 Matematikai feladat megoldása sinus tétellel
+		//       A feladat elérhetõsége: https://www.geogebra.org/m/TVa6MHE9
 
 	}
 
