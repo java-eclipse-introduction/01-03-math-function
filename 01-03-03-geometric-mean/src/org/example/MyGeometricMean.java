091834aaf3040b1466d9a48d81657c4297ad5971
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-17 21:00:25 +0100

    M3.01 Határozzuk meg két szám mértani közepét

--- 01-03-03-geometric-mean/src/org/example/MyGeometricMean.java
+++ 01-03-03-geometric-mean/src/org/example/MyGeometricMean.java
@@ -4,6 +4,9 @@ public class MyGeometricMean {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// M3.01 Határozzuk meg két szám mértani közepét
+		int a=5;
+		int b=3;	
 	}
 
 }
